8758874bfb173038322d2a13921950556b901c39
MazinhaB <mariana.barude@gec.inatel.br>
2022-08-25 11:29:51 -0300

    Cliente

--- PCMania/src/main/java/Cliente.java
+++ PCMania/src/main/java/Cliente.java
@@ -1,12 +1,14 @@
 public class Cliente {
-    String nome;
-    long cpf;
+    String nome; //nome do cliente
+    long cpf; //cpf do usuário
 
-    Computador []computadores = new Computador[10];
+    Computador []computadores = new Computador[10]; //computadores que o cliente deseja comprar
+
+    //método para calcular o preço total dos computadores
     public float calculaTotalCompra(){
         float total = 0;
         for(int i = 0; i < this.computadores.length; i++)
-            if(this.computadores[i] != null)
+            if(this.computadores[i] != null)//garante que não vai tentar somar o preço de uma posição vazia
                 total+=this.computadores[i].preco;
         return total;
     }
