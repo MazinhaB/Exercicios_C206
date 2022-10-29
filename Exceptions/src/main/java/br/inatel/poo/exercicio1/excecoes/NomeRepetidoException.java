dc362dbd80dadcf61cde27fd84bd288a18f235c7
MazinhaB <mariana.barude@gec.inatel.br>
2022-10-28 21:41:43 -0300

    Nome repetido

---
+++ Exceptions/src/main/java/br/inatel/poo/exercicio1/excecoes/NomeRepetidoException.java
@@ -0,0 +1,7 @@
+package br.inatel.poo.exercicio1.excecoes;
+
+public class NomeRepetidoException extends Exception{
+    public NomeRepetidoException(String mensagem){
+        super(mensagem);
+    }
+}
